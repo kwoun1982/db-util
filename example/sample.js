b5ef3d65a3b4c91ced6e4f6f94efaacaaed4ad1d
권기영 <kky@simplatform.com>
2016-04-29 18:14:38 +0900

    init

--- example/sample.js
+++ example/sample.js
@@ -1,52 +1,132 @@
 var DB = require("../index.js");
 DB.sqlite.config("./example/sqlite");
+var test = 3;
 
-var query = {
-    sql: "DROP TABLE USER"
-};
-console.log("=================================");
-console.log("DROP");
-console.log("=================================");
-DB.sqlite.send(query, function (err, result) {
+if (test == 1) {
 
-    console.log("err : " + err);
-    console.log("result : " + JSON.stringify(result));
-    query = {
-        sql: "CREATE TABLE USER (USER_ID TEXT, USER_PW TEXT)"
+    var query = {
+        sql: "DROP TABLE USER"
     };
-
     console.log("=================================");
-    console.log("CREATE");
+    console.log("DROP");
     console.log("=================================");
     DB.sqlite.send(query, function (err, result) {
 
         console.log("err : " + err);
         console.log("result : " + JSON.stringify(result));
-
-
-        console.log("=================================");
-        console.log("INSERT");
-        console.log("=================================");
         query = {
-            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
-            param: [["test", "pass"], ["test", "pass"]]
+            sql: "CREATE TABLE USER (USER_ID TEXT, USER_PW TEXT)"
         };
+
+        console.log("=================================");
+        console.log("CREATE");
+        console.log("=================================");
         DB.sqlite.send(query, function (err, result) {
 
             console.log("err : " + err);
             console.log("result : " + JSON.stringify(result));
 
+
+            console.log("=================================");
+            console.log("INSERT");
+            console.log("=================================");
             query = {
-                sql: "SELECT * FROM USER ",
-                param: {}
+                sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+                param: [["test", "pass"], ["test", "pass"], ["test", "pass"]]
             };
             DB.sqlite.send(query, function (err, result) {
-                console.log("=================================");
-                console.log("SELECT");
-                console.log("=================================");
-                console.log("err : "+err);
-                console.log("result : "+JSON.stringify(result));
+
+                console.log("err : " + err);
+                console.log("result : " + JSON.stringify(result));
+
+                query = {
+                    sql: "SELECT * FROM USER ",
+                    param: {}
+                };
+                DB.sqlite.send(query, function (err, result) {
+                    console.log("=================================");
+                    console.log("SELECT");
+                    console.log("=================================");
+                    console.log("err : " + err);
+                    console.log("result : " + JSON.stringify(result));
+                });
             });
         });
     });
-});
+} else {
+
+    console.log("=================================");
+    console.log("1111");
+    console.log("=================================");
+
+    querys = [
+        {
+            sql: "DROP TABLE USER"
+        },
+        {
+            sql: "CREATE TABLE USER (USER_ID TEXT, USER_PW TEXT)"
+        },
+        {
+            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+            param: [["test", "pass"], ["test", "pass"]]
+        },
+        {
+            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+            param: ["test", "pass"]
+        },
+        {
+            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+            param: [["test", "pass"], ["test", "pass"]]
+        },
+        {
+            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+            param: [["test", "pass"], ["test", "pass"]]
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        },
+        {
+            sql: "SELECT * FROM USER ",
+            param: {}
+        }
+    ];
+    DB.sqlite.sends(querys, function (err, result) {
+        console.log("err : " + err);
+        console.log("result : ");
+        console.log(result);
+    });
+
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * async
+ * db-util
  * https://github.com/kwoun1982/db-util.git
  *
  * Copyright 2016 kiyoung kwoun
@@ -51,9 +51,6 @@
             printSql = replaceAll(",", ",\n      ", printSql);
             printSql = replaceAll("FROM", "\nFROM  ", printSql);
             printSql = replaceAll("WHERE", "\nWHERE ", printSql);
-            console.log("[kky] :: ++++++++++++++++++++++++++++++++++++");
-            console.log("[kky] :: mysql.send(query, callback)");
-            console.log("[kky] :: ++++++++++++++++++++++++++++++++++++");
             console.log("\n" + printSql);
 
             // ===========================================
@@ -96,76 +93,110 @@
             }
             return sqlite_conf.conn;
         },
-        send: function (query, callback) {
-            // log.debug("[kky] :: ++++++++++++++++++++++++++++++++++++");
-            // log.debug("[kky] :: sqlite.send(query, callback)");
-            // log.debug("[kky] :: ++++++++++++++++++++++++++++++++++++");
+        multiQuery: function (db, query, callback) {
+            db.run("BEGIN TRANSACTION");
+            var stmt = db.prepare(query.sql);
+            var queryCnt = 1;
+            for (var i = 0; i < query.param.length; i++) {
+                stmt.run(query.param[i], function (err, row) {
+                    queryCnt++;
+                    if (err) {
+                        db.run("ROLLBACK");
+                        if (callback) {
+                            if (callback) {
+                                callback(err, {result: 0});
+                            }
+                            return;
+                        }
+                    }
+                    if (queryCnt == query.param.length) {
+                        if (callback) {
+                            if (callback) {
+                                callback(err, {result: query.param.length});
+                            }
+                            return;
+                        }
+                    }
+                });
+            }
+            stmt.finalize();
+            db.run("END TRANSACTION");
+        },
+        singleQuery: function (db, query, callback) {
+            db.all(query.sql, query.param, function (err, row) {
+                if (err) {
+                    console.error(err);
+                    row = {result: 0};
+                } else {
+                    if (query.sql.toUpperCase().indexOf("SELECT") > -1) {
+                    } else {
+                        // update, insert, delete
+                        if (typeof  row != "array") {
+                            row = [];
+                        }
+                        row = {result: 1};
+                    }
+                }
+                if (callback) {
+                    callback(err, row);
+                }
 
+            });
+        },
+        send: function (query, callback) {
             if (!sqlite_conf.server) {
                 throw Error("server info not found!");
             }
-
             var conn = this.createConnection();
             var db = new conn.Database(sqlite_conf.server);
             db.serialize(function () {
-                if (Array.isArray(query.param) && Array.isArray(query.param[0])) {
-                    db.run("BEGIN TRANSACTION");
-                    var stmt = db.prepare(query.sql);
-                    var queryCnt = 1;
-                    for (var i = 0; i < query.param.length; i++) {
-                        stmt.run(query.param[i], function (err, row) {
-                            queryCnt++;
-
-                            if (err) {
-                                db.run("ROLLBACK");
-                                if (callback) {
-                                    db.close(function () {
-                                        if (callback) {
-                                            callback(err, {result: 0});
-                                        }
-                                    });
-                                    return;
-                                }
-                            }
-                            if (queryCnt == query.param.length) {
-
-                                if (callback) {
-                                    db.close(function () {
-                                        if (callback) {
-                                            callback(err, {result: query.param.length});
-                                        }
-                                    });
-                                    return;
-                                }
-                            }
-                        });
-                    }
-                    stmt.finalize();
-                    db.run("END TRANSACTION");
-                } else {
-                    db.all(query.sql, query.param, function (err, row) {
-                        if (err) {
-                            console.error(err);
-                            row = {result: 0};
-
-                        } else {
-                            if (query.sql.toUpperCase().indexOf("SELECT") > -1) {
-                            } else {
-                                // update, insert, delete
-                                if (typeof  row != "array") {
-                                    row = [];
-                                }
-                                row = {result: 1};
-                            }
+                var methods = Array.isArray(query.param) && Array.isArray(query.param[0]) ? "multiQuery" : "singleQuery";
+                sqlite[methods](db, query, function (err, row) {
+                    db.close(function () {
+                        if (callback) {
+                            callback(err, row);
                         }
-                        db.close(function () {
-                            if (callback) {
-                                callback(err, row);
-                            }
-                        });
-
-
                     });
+                });
+            });
+        },
+        sends: function (querys, callback) {
+            if (!Array.isArray(querys)) {
+                callback({result: 0});
+                return;
+            }
+            var conn = this.createConnection();
+            var db = new conn.Database(sqlite_conf.server);
+            db.serialize(function () {
+                sqlite_conf.multiCnt = null;
+                sqlite.sendsSql(db, querys, function (err, rows) {
+                    db.close(function () {
+                        callback(err, rows);
+                    });
+                });
+            });
+        },
+        sendsSql: function (db, querys, callback) {
+            if (sqlite_conf.multiCnt == null) {
+                sqlite_conf.multiCnt = 0;
+                sqlite_conf.callback = callback;
+                sqlite_conf.rows = [];
+            }
+            var query = querys[sqlite_conf.multiCnt];
+            var methods = Array.isArray(query.param) && Array.isArray(query.param[0]) ? "multiQuery" : "singleQuery";
+            sqlite[methods](db, query, function (err, row) {
+
+                sqlite_conf.rows.push(row);
+
+                sqlite_conf.multiCnt++;
+                query = querys[sqlite_conf.multiCnt];
+
+                if (sqlite_conf.multiCnt > querys.length - 1) {
+                    if (sqlite_conf.callback) {
+                        sqlite_conf.callback(err, sqlite_conf.rows);
+                    }
+                } else {
+                    sqlite.sendsSql(db, querys);
                 }
             });
         }
