695cfa9c06a9a44ebfd5471fb0df0981f1d5ca89
권기영 <kky@simplatform.com>
2016-05-03 15:32:49 +0900

    init

--- example/sample.js
+++ example/sample.js
@@ -1,9 +1,80 @@
 var DB = require("../index.js");
 DB.sqlite.config("./example/sqlite");
-var test = 3;
+var test = 2;
+
+var querys = [
+    {
+        sql: "DROP TABLE USER"
+    },
+    {
+        sql: "CREATE TABLE USER (USER_ID TEXT, USER_PW TEXT)"
+    },
+    {
+        sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+        param: [["test", "pass"], ["test", "pass"]]
+    },
+    {
+        sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+        param: ["test", "pass"]
+    },
+    {
+        sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+        param: [["test", "pass"], ["test", "pass"]]
+    },
+    {
+        sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
+        param: [["test", "pass"], ["test", "pass"]]
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    },
+    {
+        sql: "SELECT * FROM USER ",
+        param: {}
+    }
+];
+
 
 if (test == 1) {
-
+    DB.sqlite.sends(querys, function (err, result) {
+        console.log("err : " + err);
+        console.log("result : ");
+        console.log(result);
+    });
+} else {
     var query = {
         sql: "DROP TABLE USER"
     };
@@ -32,7 +103,7 @@ if (test == 1) {
             console.log("=================================");
             query = {
                 sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
-                param: [["test", "pass"], ["test", "pass"], ["test", "pass"]]
+                param: [["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"],["test", "pass"], ["test", "pass"], ["test", "pass"]]
             };
             DB.sqlite.send(query, function (err, result) {
 
@@ -53,80 +124,4 @@ if (test == 1) {
             });
         });
     });
-} else {
-
-    console.log("=================================");
-    console.log("1111");
-    console.log("=================================");
-
-    querys = [
-        {
-            sql: "DROP TABLE USER"
-        },
-        {
-            sql: "CREATE TABLE USER (USER_ID TEXT, USER_PW TEXT)"
-        },
-        {
-            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
-            param: [["test", "pass"], ["test", "pass"]]
-        },
-        {
-            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
-            param: ["test", "pass"]
-        },
-        {
-            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
-            param: [["test", "pass"], ["test", "pass"]]
-        },
-        {
-            sql: "INSERT INTO USER (USER_ID, USER_PW) VALUES (?,?)",
-            param: [["test", "pass"], ["test", "pass"]]
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        },
-        {
-            sql: "SELECT * FROM USER ",
-            param: {}
-        }
-    ];
-    DB.sqlite.sends(querys, function (err, result) {
-        console.log("err : " + err);
-        console.log("result : ");
-        console.log(result);
-    });
-
 }
